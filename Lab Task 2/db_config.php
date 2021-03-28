d4ea651fa934b3b5b0a76fa927da61c6b8122fd2
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2021-03-28 22:33:05 +0600

    Lab Task 2

--- Lab Task 2/db_config.php
+++ Lab Task 2/db_config.php
@@ -2,7 +2,7 @@
 	$db_server="localhost";
 	$db_user="root";
 	$db_pass="";
-	$db_name="webtec";
+	$db_name="wt_sp21_l";
 	
 	function execute($query){   //running insert,update,delete
 		global $db_server,$db_user,$db_pass,$db_name;
